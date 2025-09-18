1321d97218794023978bb72e80df61c40c5116d8
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-09-18 18:10:18 +0900

    init

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,88 +1,93 @@
 const express = require("express")
 const router = express.Router()
-const jwt = require("jsonwebtoken")
+
+const bcrypt = require("bcrypt")
 const Contact = require('../models/Contact')
 
-
-router.post('/', async (req, res) => {
+router.post('/',async(req,res)=>{
     try {
-        const { name, email, phone, message, status } = req.body
+        const {name, email, phone, message,status}=req.body
 
         const contact = new Contact({
-            name,
-            email,
-            phone,
+            name, 
+            email, 
+            phone, 
             message,
             status
         })
         await contact.save()
 
-        res.status(201).json({ message: "문의가 성공적으로 등록" })
+        res.status(201).json({message:"문의가 성공적으로 등록"})
     } catch (error) {
         console.log(error)
-        res.status(500).json({ message: '서버에러' })
+        res.status(500).json({message:'서버에러'})
     }
 })
-router.get('/', async (req, res) => {
-    try {
 
+router.get('/',async(req,res)=>{
+    try {
         const contacts = await Contact.find().sort({
             createdAt:-1
         })
-
         res.json(contacts)
-      
     } catch (error) {
         console.log(error)
-        res.status(500).json({ message: '서버에러' })
+        res.status(500).json({message:'서버에러'})
     }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id',async(req,res)=>{
     try {
-        
+
         const contact = await Contact.findById(req.params.id)
+
         if(!contact){
-            return res.status(404).json({message:'문의글을 찾을 수 없음'})
+            return res.status(404).json({message:"문의글을 찾을 수 없음"})
         }
         res.json(contact)
-      
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: '서버에러' })
-    }
-})
-router.put('/:id', async (req, res) => {
-    try {
-        const { status }=req.body
-        const contact = await Contact.findByIdAndUpdate(
-            req.params.id,
-            { status },
-            {new:true}
-        )
-        if(!contact){
-            return res.status(404).json({message:'문의글을 찾을 수 없음'})
-        }
-        res.json({message:"문의 상태 성공적 수정",contact})
-      
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: '서버에러' })
-    }
-})
-router.delete('/:id', async (req, res) => {
-    try {
         
-        const contact = await Contact.findByIdAndDelete(req.params.id)
-        if(!contact){
-            return res.status(404).json({message:'문의글을 찾을 수 없음'})
-        }
-        res.json({message:"문의글 성공적 삭제"})
-      
     } catch (error) {
         console.log(error)
-        res.status(500).json({ message: '서버에러' })
+        res.status(500).json({message:'서버에러'})
     }
 })
 
-module.exports = router
+router.put('/:id',async(req,res)=>{
+    try {
+        const {status} = req.body
+        const contact = await Contact.findByIdAndUpdate(
+            req.params.id,
+            {status},
+            {new:true}
+        )
+
+        if(!contact){
+            return res.status(404).json({message:"문의글을 찾을 수 없음"})
+        }
+        res.json({message : "문의 상태 성공적 수정 " ,contact})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:'서버에러'})
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    try {
+
+        const contact = await Contact.findByIdAndDelete(req.params.id)
+
+        if(!contact){
+            return res.status(404).json({message:"문의글을 찾을 수 없음"})
+        }
+        res.json({message:"문의글 성공적 삭제"})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:'서버에러'})
+    }
+})
+
+
+
+module.exports=router
--- frontend/src/components/admin/AdminContact.jsx
+++ frontend/src/components/admin/AdminContact.jsx
@@ -4,74 +4,139 @@ import { api } from '../../lib/api'
 import Swal from 'sweetalert2'
 const AdminContact = () => {
 
-  const [contacts, setContacts] = useState([])
+    const [contacts, setContacts] = useState([])
 
-  useEffect(() => {
+    useEffect(() => {
 
-    const fetchData = async () => {
-      try {
-        const res = await api.get('/api/contact')
-        setContacts(res.data)
+        const fetchData = async () => {
+            try {
+                const res = await api.get('/api/contact')
+                setContacts(res.data)
 
-      } catch (error) {
-        console.log('문의글 가져오기 실패', error)
-      }
+            } catch (error) {
+                console.log('문의글 가져오기 실패', error)
+            }
+        }
+
+        fetchData()
+
+    }, [])
+
+    const handleStatusUpdate = async (contactId, newStatus) => {
+        try {
+            await api.put(
+                `/api/contact/${contactId}`,
+                { status: newStatus },
+                { withCredentials: true }
+            )
+
+            setContacts((prev) =>
+                prev.map((c) => (c._id === contactId ? { ...c, status: newStatus } : c))
+            )
+            Swal.fire("수정완료", "상태가 성공적으로 수정되었습니다.", "success")
+        } catch (error) {
+            console.log("수정실패", error)
+            Swal.fire("오류 발생", "수정중 오류가 발생했습니다.", "error")
+        }
     }
 
-    fetchData()
+    const showStatusChangeModal = async (contact) => {
+        const { value: newStatus, isConfirmed } = await Swal.fire({
+            title: "문의 상태 수정",
+            input: "radio",
+            inputOptions: {
+                "pending": "대기중",
+                "inprogress": "진행중",
+                "completed": "완료"
+            },
+            inputValue: contact.status,
+            confirmButtonText: "적용하기",
+            cancelButtonText: "취소",
+            showCancelButton: true
+        })
 
-  }, [])
+        if (isConfirmed && newStatus) {
+            handleStatusUpdate(contact._id, newStatus)
+        }
+    }
 
-  return (
-    <div className='inner admin-contact'>
-      <h2>문의글 관리</h2>
-      <div className="contact-wrapper">
-        <ul className="contact-list">
-          {contacts.map((c, i) => (
+    const handleDelete = async (id) => {
+        const result = await Swal.fire({
+            title: "삭제하시겠습니까?",
+            text: "이 작업은 되돌릴 수 없습니다.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "삭제",
+            cancelButtonText: "취소",
+        })
 
-            <li key={c._id}>
-              <p>
-                <span className='key'>
-                  이름
-                </span>
-                :
-                <span className='value'>
-                  {c.name}
-                </span>
-              </p>
-              <p>
-                <span className='key'>email</span>
-                :
-                <span className='value'>
-                  {c.email}
-                </span>
-              </p>
-              <p>
-                <span className='key'>전화번호</span>:
-                <span className='value'>
-                  {c.phone}
-                </span>
-              </p>
-              <p><span className='key'>메세지</span></p>
-              <p className='message'>
-                {c.message}
-              </p>
-              <p>
-                <span className='key'>상태</span>
-                <span className={`status s-${(c.status||"").replace(" ","-")}`}>
-                  {c.status}
-                </span>
-              </p>
-              <div className="btns">
-                <button>상태변경</button>
-                <button>삭제</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+        if(!result.isConfirmed) return;
+
+        try {
+            await api.delete(`/api/contact/${id}`,{
+                withCredentials:true
+            })
+                setContacts((prev)=>prev.filter((c)=>c._id !==id))
+                Swal.fire("삭제 완료","문의가 성공적으로 삭제되었습니다.","success")
+        } catch (error) {
+            console.log("삭제 실패",error)
+            Swal.fire("오류발생","삭제 중 문제가 발생하였습니다.","error")
+            
+        }
+    }
+
+    return (
+        <div className='inner admin-contact'>
+            <h2>문의글 관리</h2>
+            <div className="contact-wrapper">
+                <ul className="contact-list">
+                    {contacts.map((c) => (
+
+                        <li key={c._id}>
+                            <p>
+                                <span className='key'>
+                                    이름
+                                </span>
+                                :
+                                <span className='value'>
+                                    {c.name}
+                                </span>
+                            </p>
+                            <p>
+                                <span className='key'>email</span>
+                                :
+                                <span className='value'>
+                                    {c.email}
+                                </span>
+                            </p>
+                            <p>
+                                <span className='key'>전화번호</span>:
+                                <span className='value'>
+                                    {c.phone}
+                                </span>
+                            </p>
+                            <p><span className='key'>메세지</span></p>
+                            <p className='message'>
+                                {c.message}
+                            </p>
+                            <p>
+                                <span className='key'>상태</span>
+                                <span className={`status s-${(c.status || "").replace(" ", "-")}`}>
+                                    {c.status}
+                                </span>
+                            </p>
+                            <div className="btns">
+                                <button onClick={() => showStatusChangeModal(c)}>상태변경</button>
+                                <button onClick={()=>handleDelete(c._id)}>삭제</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default AdminContact
